0d80a4c773bb2ed6202139201858e262530b568c
Gabin <gazette9@naver.com>
2024-04-21 14:10:52 +0900

    Title: 문자열 받아 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //문자열 입력
+        String s = sc.next();
+
+        //출력
+        System.out.print(s);
+    }
+}
