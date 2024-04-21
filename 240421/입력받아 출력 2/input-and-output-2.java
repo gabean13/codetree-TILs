fa7fe86fd36536776f542948ac6a161d9612b444
Gabin <gazette9@naver.com>
2024-04-21 14:32:17 +0900

    Title: 입력받아 출력 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.print(str1+str2);
+    }
+}
