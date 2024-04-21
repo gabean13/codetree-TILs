0a8f7bc6fe799f040bb8cb2918c471861ef0680f
Gabin <gazette9@naver.com>
2024-04-21 13:34:48 +0900

    Title: 입력받아 계산 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/입력받아 계산 2/input-calculate-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //정수 a, b 입력
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        //a*b 출력
+        System.out.print(a*b);
+    }
+}
