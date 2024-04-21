68eb25abb233183b8315a71465dcd580a078fa27
Gabin <gazette9@naver.com>
2024-04-21 14:09:04 +0900

    Title: 문자 받아 출력, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //문자 C입력
+        char c = sc.next().charAt(0);
+        
+        //출력
+        System.out.print(c);
+    }
+}
