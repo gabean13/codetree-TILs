f6d455c97c2723d7928b91f81eaf1528e02b1069
Gabin <gazette9@naver.com>
2024-04-22 23:24:46 +0900

    Title: 2개의 정수를 서로 더하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        a += b;
+        b += a;
+
+        System.out.print(a + " " + b);
+    }
+}
