e96aefe3fe1b0988d7a60662bf602b4fd2cb8591
Gabin <gazette9@naver.com>
2024-04-22 23:20:33 +0900

    Title: 몫과 나머지, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/몫과 나머지/quotient-remainder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.printf("%d...%d", a/b, a%b);
+    }
+}
