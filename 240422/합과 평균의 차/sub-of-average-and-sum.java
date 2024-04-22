e1b18d122b5e30cd49d08812b43d2909e5b1d8f0
Gabin <gazette9@naver.com>
2024-04-22 23:41:59 +0900

    Title: 합과 평균의 차, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        long sum = a + b + c;
+
+        System.out.println(sum);
+        System.out.println(sum/3);
+        System.out.println(sum - sum/3);
+
+    }
+}
