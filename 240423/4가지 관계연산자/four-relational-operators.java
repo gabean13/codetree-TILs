f92cde81e017ce2089affaca62e540529f768cc2
Gabin <gazette9@naver.com>
2024-04-23 21:54:41 +0900

    Title: 4가지 관계연산자, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        printValue((a>=b));
+        printValue((a>b));
+        printValue((a<=b));
+        printValue((a<b));
+
+    }
+
+    public static void printValue(boolean bool){
+        if(bool){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
