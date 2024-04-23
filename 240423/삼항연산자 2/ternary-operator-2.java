e40e89268218a77310823e1add25706579a013ee
Gabin <gazette9@naver.com>
2024-04-23 13:57:51 +0900

    Title: 삼항연산자 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String isA = sc.nextLong() == 1 ? "t" : "f";
+
+        System.out.println(isA);
+    }
+}
