4b77d1f58b42c7f8be5e41bb8e7f11fc3842ac25
Gabin <gazette9@naver.com>
2024-04-23 14:04:41 +0900

    Title: 살 수 있는 물건, Time: 202ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long money = sc.nextLong();
+
+        if(money >= 3000){
+            System.out.println("book");
+        }else if(money >= 1000){
+            System.out.println("mask");
+        }else{
+            System.out.println("no");
+        }
+    }
+}
