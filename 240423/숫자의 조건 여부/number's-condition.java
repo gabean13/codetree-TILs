d3d0e58e2333d592fc3f9d8a906ed3ad9597a656
Gabin <gazette9@naver.com>
2024-04-23 13:47:44 +0900

    Title: 숫자의 조건 여부, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+
+        if(a >= 113){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
