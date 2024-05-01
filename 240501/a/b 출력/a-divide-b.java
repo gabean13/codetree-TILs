0b4920e44ca7289d387742700493453b00c37963
Gabin <gazette9@naver.com>
2024-05-01 14:12:46 +0900

    Title: a/b 출력, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/a/b 출력/a-divide-b.java
+++ 240501/a/b 출력/a-divide-b.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.print( a/b + ".");
 
 
-        for(int i = 0; i < 21; i++){
+        for(int i = 0; i < 20; i++){
             a %= b;
             a *= 10;
             System.out.print(a/b);
